59dc471013884e9bd2dda231592baf33e4d902d7
Tutorat <57213812+n-tran117@users.noreply.github.com>
2023-07-24 11:24:29 +0200

    Maj nom de fonctions moyennes

--- Program.cs
+++ Program.cs
@@ -43,14 +43,14 @@ namespace TPMoyennes
             Eleve eleve = sixiemeA.eleves[6];
             // Afficher la moyenne d'un élève dans une matière
             Console.Write(eleve.prenom + " " + eleve.nom + ", Moyenne en " + sixiemeA.matieres[1] + " : " +
-            eleve.Moyenne(1) + "\n");
+            eleve.moyenneMatiere(1) + "\n");
             // Afficher la moyenne générale du même élève
-            Console.Write(eleve.prenom + " " + eleve.nom + ", Moyenne Generale : " + eleve.Moyenne() + "\n");
+            Console.Write(eleve.prenom + " " + eleve.nom + ", Moyenne Generale : " + eleve.moyenneGeneral() + "\n");
             // Afficher la moyenne de la classe dans une matière
             Console.Write("Classe de " + sixiemeA.nomClasse + ", Moyenne en " + sixiemeA.matieres[1] + " : " +
-            sixiemeA.Moyenne(1) + "\n");
+            sixiemeA.moyenneMatiere(1) + "\n");
             // Afficher la moyenne générale de la classe
-            Console.Write("Classe de " + sixiemeA.nomClasse + ", Moyenne Generale : " + sixiemeA.Moyenne() + "\n");
+            Console.Write("Classe de " + sixiemeA.nomClasse + ", Moyenne Generale : " + sixiemeA.moyenneGeneral() + "\n");
             Console.Read();
         }
     }
