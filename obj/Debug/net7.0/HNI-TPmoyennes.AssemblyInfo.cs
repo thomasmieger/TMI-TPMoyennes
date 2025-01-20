49f83762c1356da48b84612ffe2a8afaaa364415
thomasmieger <thomas.mieger@hotmail.com>
2025-01-20 10:18:42 +0100

    First Push

--- obj/Debug/net7.0/HNI-TPmoyennes.AssemblyInfo.cs
+++ obj/Debug/net7.0/HNI-TPmoyennes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HNI-TPmoyennes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+662f6a628df8bc910a7360277fea24a166032840")]
 [assembly: System.Reflection.AssemblyProductAttribute("HNI-TPmoyennes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HNI-TPmoyennes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
